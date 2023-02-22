88130c057e0c2c7f6337cb3a027e2844c3de07b8
FalconGV <falcongvv@gmail.com>
2023-02-22 15:38:13 +0200

    valid79

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,12 +14,12 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'testproject8849@gmail.com';                 // Наш логин
-$mail->Password = 'iwxujkriufblphfn';                           // Наш пароль от ящика
+$mail->Password = 'Test8849%';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('testproject8849@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('vitshostak78@gmail.com');     // Add a recipient
+$mail->addAddress('testproject8849@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
